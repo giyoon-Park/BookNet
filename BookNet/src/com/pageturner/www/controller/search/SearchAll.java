34c9a2ab7fef3dbb5af8ddaf52e850b7be33e5eb
user <user@125.133.201.186>
2020-05-25 00:04:50 +0900

    2020 create jsp, class- seo

---
+++ BookNet/src/com/pageturner/www/controller/search/SearchAll.java
@@ -0,0 +1,16 @@
+package com.pageturner.www.controller.search;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pageturner.www.controller.PageController;
+
+public class SearchAll implements PageController {
+
+	@Override
+	public String exec(HttpServletRequest req, HttpServletResponse resp) {
+
+		return null;
+	}
+
+}
